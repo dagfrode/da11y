3f25ab4f29d9ca163e4908ea234abc8b7e0ea520
Dag Frode Solberg <dag.solberg@sparebank1.no>
2024-05-29 10:43:18 +0200

    Add some styling

--- src/CheckList.tsx
+++ src/CheckList.tsx
@@ -1,3 +1,5 @@
+import styles from "./checklist.module.css";
+
 export const CheckList = () => {
   return (
     <>
@@ -14,8 +16,11 @@ export const CheckList = () => {
 
 export const CheckListItem = ({ title, children }) => {
   return (
-    <details>
-      <summary>
+    <details style={{ padding: "5px 10px" }}>
+      <summary
+        style={{ padding: " 10px", backgroundColor: "lightblue" }}
+        className={styles.summary}
+      >
         <label>
           <input type="checkbox" />
           {title}
@@ -26,14 +31,35 @@ export const CheckListItem = ({ title, children }) => {
   );
 };
 
+//TODO
+// Turn website greyscale to see if it makes sence without color
+// Turn website blury to simulate bad vision
+// Show areia and alt labes
+// show tags on text for h1-6 and p to help show if the structure is alright
+// how to test keyboard?
+// How to test form?
+// Look trough wcag to find ways of testing each relevant requirement and add it to the list
+
 export const KeyBoard = ({}) => {
-  return <CheckListItem title="tastatur">
-    <CheckListItem title="Skip">derp</CheckListItem>
-  </CheckListItem>;
+  return (
+    <CheckListItem title="tastaturr">
+      <CheckListItem title="Skip">derp</CheckListItem>
+    </CheckListItem>
+  );
 };
 
 export const Scale = ({}) => {
-  return <CheckListItem title="forstørring"></CheckListItem>;
+  return (
+    <CheckListItem title="forstørring">
+      <CheckListItem title=" font forstørring">
+        Skaller opp fonten til 200% - du kan bruke shift + scroll (feature added
+        by da11y)
+      </CheckListItem>
+      <CheckListItem title="nettside forstørring">
+        Skaller opp nettsiden - du kan bruke ctrl + scroll
+      </CheckListItem>
+    </CheckListItem>
+  );
 };
 
 export const Structure = ({}) => {
@@ -45,13 +71,30 @@ export const Form = ({}) => {
 };
 
 export const AutomatedTesting = ({}) => {
-  return <CheckListItem title="automatiserte verktøy"></CheckListItem>;
+  return (
+    <CheckListItem title="automatiserte verktøy">
+      <CheckListItem title="lint">eslint ++</CheckListItem>
+      <CheckListItem title="component">react testing library</CheckListItem>
+      <CheckListItem title="integration">playwright</CheckListItem>
+      <CheckListItem title="extention">wave</CheckListItem>
+    </CheckListItem>
+  );
 };
 
 export const ScreenReader = ({}) => {
-  return <CheckListItem title="skjermlesing"></CheckListItem>;
+  return (
+    <CheckListItem title="skjermlesing">
+      <CheckListItem title="accessability tree"></CheckListItem>
+      <CheckListItem title="skjermleser">- mac - linux - windows</CheckListItem>
+    </CheckListItem>
+  );
 };
 
 export const UserTesting = ({}) => {
-  return <CheckListItem title="brukertesting"></CheckListItem>;
+  return (
+    <CheckListItem title="brukertesting">
+      <CheckListItem title="gjevnlig review"></CheckListItem>
+      <CheckListItem title="brukertesting med faktiske brukere"></CheckListItem>
+    </CheckListItem>
+  );
 };
--- src/Control.tsx
+++ src/Control.tsx
@@ -1,42 +1,48 @@
-import React from 'react'
-import { createPortal } from 'react-dom';
-import { CheckList } from './CheckList';
+import React from "react";
+import { createPortal } from "react-dom";
+import { CheckList } from "./CheckList";
 
 class Control extends React.Component {
   constructor(props: any) {
     super(props);
     // STEP 1: create a container <div>
-    this.containerEl = document.createElement('div');
-    this.containerEl.id ="test"
+    this.containerEl = document.createElement("div");
+    this.containerEl.id = "test";
     this.externalWindow = null;
   }
 
-    componentDidMount() {
-
-       // STEP 3: open a new browser window and store a reference to it
-    this.externalWindow = window.open('', '', 'width=600,height=400,left=200,top=200');
+  componentDidMount() {
+    // STEP 3: open a new browser window and store a reference to it
+    this.externalWindow = window.open(
+      "",
+      "",
+      "width=600,height=400,left=200,top=200"
+    );
 
     // STEP 4: append the container <div> (that has props.children appended to it) to the body of the new window
     this.externalWindow.document.body.appendChild(this.containerEl);
-    }
-
-
-  
-   
-  
-    render() {
-      return createPortal(
-    <CheckList />
-      , this.containerEl);
-    }
-
-    componentWillUnmount() {
-      // STEP 5: This will fire when this.state.showWindowPortal in the parent component becomes false
-      // So we tidy up by closing the window
-      this.externalWindow.close();
-    }
   }
 
+  toggleGreyScale = () => {
+    document.body.classList.add("greyscale");
+    console.log(document.body.style.filter);
+  };
 
+  render() {
+    return createPortal(
+      <>
+        <CheckList />
+        <button onClick={this.toggleGreyScale}>greyscale</button>
+      </>,
+      this.containerEl
+    );
+  }
 
-export default Control
+  componentWillUnmount() {
+    // STEP 5: This will fire when this.state.showWindowPortal in the parent component becomes false
+    // So we tidy up by closing the window
+    this.externalWindow.close();
+  }
+}
+
+export default Control;
