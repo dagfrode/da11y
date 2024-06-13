47e820fc13d441192bda3b36e9e6fb7dc46ca312
Dag Frode Solberg <dag.solberg@sparebank1.no>
2024-06-13 17:55:32 +0200

    Add element type and disable scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,183 @@
-import React from 'react';
-import Control from './Control';
+import React from "react";
+import Control from "./Control";
 
 class App extends React.Component {
-
   constructor(props: any) {
     super(props);
     const baseFontSize = parseInt(
       getComputedStyle(document.documentElement, null)["font-size"]
     );
 
-    this.state = { controlsOpen  :true, fontScale: 100, baseFontSize };
-  }
-
-
-    componentDidMount() {
-      document.addEventListener('wheel', this.handleKeyDown);
-      document.addEventListener('keyup', this.handleKeyUp);
-
- }
-
-// TODO - show feedback in main window
-// fill out the rest of the checklist from the video
-  
-    handleKeyDown = (event: WheelEvent) => {
-      if (!event.shiftKey) return;
-      event.preventDefault();
-      
-      let scale = this.state.fontScale
-      if (event.wheelDelta > 0) {
-        scale += 25;
-      } else {
-        scale -= 25;
-      }
-      
-      this.setState({...this.state, fontScale: scale})
-      
-      console.log("Font scale", (this.state.baseFontSize * scale) / 100 + "px");
-
-
-         document.documentElement.style.fontSize = (this.state.baseFontSize * scale) / 100 + "px";
-        
-        // el.innerHTML = `${scaling.toString()}%`;
-        // document.body.appendChild(el);
-        // timer = Date.now();
-        // setTimeout(() => {
-        //   if (Date.now() - timer > 800 && document.body.contains(el)) {
-        //     document.body.removeChild(el);
-        //   }
-        // }, 1000);
-    
-      }
-
-    
-
-      handleKeyUp=(event: KeyboardEvent) =>{
-        console.log('Key pressed: ', event.key, event.ctrlKey, event.shiftKey);
-        if(event.ctrlKey && event.shiftKey && event.key === "U"){
-          this.setState({...this.state,controlsOpen: !this.state.controlsOpen})
-        }
-  
-      }
-  
-    render() {
-      return <div>Press any key!
-       { this.state.controlsOpen && <Control />}
-      </div>;
+    this.style = document.createElement("style");
+    this.style.innerHTML = `
+    body {
+      color: red;
+    }
+    .greyscale {
+      filter: grayscale(1);
     }
 
- 
+    .elementType h1::before{
+      content: '<H1>'
+    }
+    .elementType h2::before{
+      content: '<H2>'
+    }   
+    .elementType h3::before{
+      content: '<H3>'
+    }
+    .elementType h4::before{
+      content: '<H4>'
+    }
+    .elementType h5::before{
+      content: '<H5>'
+    }
+    .elementType h6::before{
+      content: '<H6>'
+    }
+    .elementType a::before{
+      content: '<A(Link)>'
+    }
+    .elementType button::before{
+      content: '<Button>'
+    }
+    .elementType ul::before{
+      content: '<UnorderedList>'
+    }
+    .elementType li::before{
+      content: '<ListItem>'
+    }
+    .elementType ol::before{
+      content: '<OrderedList>'
+    }
+    .elementType p::before{
+      content: '<P>'
+    }
+    .elementType label::before{
+      content: '<Label>'
+    }
+    .elementType select::before{
+      display:block;
+      content: '<Select>'
+    }
+    .elementType input::before{
+      content: '<Input>'
+    }
+    .elementType textarea::before{
+      content: '<Teatarea>'
+    }
+    .elementType table::before{ // works?
+      content: '<Table>'
+    }
+    .elementType section::before{
+      content: '<Section>'
+    }
+  `;
+
+    this.state = {
+      greyscale: false,
+      controlsOpen: true,
+      fontScale: 100,
+      baseFontSize,
+      elementType: false,
+      disableStyles: false,
+    };
   }
 
+  componentDidMount() {
+    document.addEventListener("wheel", this.handleKeyDown);
+    document.addEventListener("keyup", this.handleKeyUp);
+    document.head.appendChild(this.style);
+  }
 
+  // TODO - show feedback in main window
+  // fill out the rest of the checklist from the video
 
-export default App
+  handleKeyDown = (event: WheelEvent) => {
+    if (!event.shiftKey) return;
+    event.preventDefault();
+
+    let scale = this.state.fontScale;
+    if (event.wheelDelta > 0) {
+      scale += 25;
+    } else {
+      scale -= 25;
+    }
+
+    this.setState({ ...this.state, fontScale: scale });
+
+    console.log("Font scale", (this.state.baseFontSize * scale) / 100 + "px");
+
+    document.documentElement.style.fontSize =
+      (this.state.baseFontSize * scale) / 100 + "px";
+
+    // el.innerHTML = `${scaling.toString()}%`;
+    // document.body.appendChild(el);
+    // timer = Date.now();
+    // setTimeout(() => {
+    //   if (Date.now() - timer > 800 && document.body.contains(el)) {
+    //     document.body.removeChild(el);
+    //   }
+    // }, 1000);
+  };
+
+  toggleDisableStyles = () => {
+    if (this.state.disableStyles) {
+      document
+        .querySelectorAll('style, link[rel="stylesheet"]')
+        .forEach((e) => e.removeAttribute("disabled"));
+    } else {
+      document
+        .querySelectorAll('style, link[rel="stylesheet"]')
+        .forEach((e) => e.setAttribute("disabled", "disabled"));
+    }
+    this.setState({
+      ...this.state,
+      disableStyles: !this.state.disableStyles,
+    });
+  };
+
+  toggleGreyScale = () => {
+    this.toggleProperty("greyscale");
+  };
+
+  toggleElementType = () => {
+    this.toggleProperty("elementType");
+  };
+
+  toggleProperty = (property: "elementType" | "greyscale") => {
+    if (this.state[property]) {
+      document.body.classList.remove(property);
+    } else {
+      document.body.classList.add(property);
+    }
+    this.setState({
+      ...this.state,
+      [property]: !this.state[property],
+    });
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    console.log("Key pressed: ", event.key, event.ctrlKey, event.shiftKey);
+    if (event.ctrlKey && event.shiftKey && event.key === "U") {
+      this.setState({ ...this.state, controlsOpen: !this.state.controlsOpen });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        Press any key!
+        {this.state.controlsOpen && (
+          <Control
+            toggleGreyScale={this.toggleGreyScale}
+            toggleElementType={this.toggleElementType}
+            toggleDisableStyles={this.toggleDisableStyles}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/Control.tsx
+++ src/Control.tsx
@@ -13,26 +13,19 @@ class Control extends React.Component {
 
   componentDidMount() {
     // STEP 3: open a new browser window and store a reference to it
-    this.externalWindow = window.open(
-      "",
-      "",
-      "width=600,height=400,left=200,top=200"
-    );
+    this.externalWindow = window.open("", "", "width=1024,height=700");
 
     // STEP 4: append the container <div> (that has props.children appended to it) to the body of the new window
     this.externalWindow.document.body.appendChild(this.containerEl);
   }
 
-  toggleGreyScale = () => {
-    document.body.classList.add("greyscale");
-    console.log(document.body.style.filter);
-  };
-
   render() {
     return createPortal(
       <>
         <CheckList />
-        <button onClick={this.toggleGreyScale}>greyscale</button>
+        <button onClick={this.props.toggleGreyScale}>greyscale</button>
+        <button onClick={this.props.toggleElementType}>elementType</button>
+        <button onClick={this.props.toggleDisableStyles}>disableStyles</button>
       </>,
       this.containerEl
     );
